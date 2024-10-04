ea21ee07a52c68e341534e0021073f0323d276c7
fahimhossen <fahim.hossen@koneksys.com>
2024-10-04 17:40:08 +0600

    update url

--- src/component/Auth/Login.js
+++ src/component/Auth/Login.js
@@ -5,7 +5,8 @@ import "./Login.css"; // Add your CSS here
 const LinkedInLogin = () => {
   // LinkedIn App Credentials
   const CLIENT_ID = "860d5bd232a4cu";
-  const REDIRECT_URI = "https://storied-naiad-4fe8c6.netlify.app/auth/callback";
+  const REDIRECT_URI =
+    "https://linked-in-sso-ptxnvm438-smfahim25s-projects.vercel.app/auth/callback";
   const AUTH_URL = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&state=foobar&scope=openid%20profile%20email`;
   const handleLinkedInLogin = () => {
     window.location.reload();
